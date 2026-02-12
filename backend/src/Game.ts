8289ea4d76b1dbb2b7c2216f148a67f5977b1171
Sanket Kharat <sanketkharat28@gmail.com>
2026-02-12 21:24:16 +0530

    implement makemove function, validates turn of a player and checks if the move made by the player is valid

--- backend/src/Game.ts
+++ backend/src/Game.ts
@@ -1,17 +1,37 @@
+import { Chess } from "chess.js";
 import { WebSocket } from "ws";
 
 export class Game {
   private player1: WebSocket;
   private player2: WebSocket;
-  private board: string;
-  private moves: string[];
+  private board: Chess;
   private startTime: Date;
 
   constructor(player1: WebSocket, player2: WebSocket) {
     this.player1 = player1;
     this.player2 = player2;
-    this.board = "";
-    this.moves = [];
+    this.board = new Chess();
     this.startTime = new Date();
+  }
+
+  makeMove(socket:WebSocket, move: {
+    from: string;
+    to: string
+  }) {
+
+    if(this.board.moves.length % 2 === 0 && socket !== this.player1) {
+        return;
+    }
+
+    if(this.board.moves.length % 2 === 1 && socket !== this.player2) {
+        return;
+    }
+
+    try {
+        this.board.move(move);
+    } catch(e) {
+        return;
+    }
+    
   }
 }
