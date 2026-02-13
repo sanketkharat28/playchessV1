894b7541d3313c649d594323d24fbea119f520ce
Sanket Kharat <sanketkharat28@gmail.com>
2026-02-13 16:19:16 +0530

    create button component

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,5 +1,20 @@
+import { Button } from "../components/Button"
+import { ChessBoard } from "../components/ChessBoard"
+
 export const Game = () => {
-    return <div>
-        Game page
+    return <div className="justify-center flex">
+        <div className="pt-8 max-w-screen-lg w-full">
+               <div className="grid grid-cols-6 gap-4 w-full">
+                <div className="col-span-4 bg-red-200 w-full">
+                    <ChessBoard/>
+                </div>
+                <div className="col-span-2 bg-green-200 w-full">
+                    <Button onClick={()=> {
+                        navigate("/game")}}>
+                        Play
+                    </Button>
+                </div>
+               </div>
+        </div>
     </div>
 }
--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "../components/Button";
+
 export const Landing = () => {
+    const navigate = useNavigate();
     return <div className="flex justify-center">
         <div className="pt-8 max-w-screen-lg">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
@@ -12,9 +16,10 @@ export const Landing = () => {
                 </h1>
             </div>
             <div className="mt-4 flex justify-center">
-                <button className="px-8 py-0 text-2xl bg-green-500 hover:bg-green-700 text-white font-bold rounded">
-                    Play Online
-                </button>
+              <Button onClick={()=> {
+                navigate("/game")}}>
+                    Play
+                </Button>
             </div>
         </div>
         
