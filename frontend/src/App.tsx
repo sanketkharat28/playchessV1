0a979f9a9dc0d82fddd6a5da06bfce39796039a8
Sanket Kharat <sanketkharat28@gmail.com>
2026-02-23 15:09:50 +0530

    fix: remove unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Landing } from './screens/Landing'
 import { Game } from './screens/Game'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='h-screen bg-slate-950'>
        <BrowserRouter>
